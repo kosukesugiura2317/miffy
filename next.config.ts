cc656238ab09a146491dfdebe2ea5dd3755ac654
kosuke sugiura <kosuke@kosukenoMacBook-Pro.local>
2026-01-30 04:12:01 +0900

    chore: add github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "miffy";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
